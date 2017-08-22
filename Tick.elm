ccd8716a621a3dcecd4b5bdd69221397ef4c4862
Palmer Paul <pzpaul2002@yahoo.com>
2017-08-22 09:14:53 -0400

    update Tick.elm with new API

--- Tick.elm
+++ Tick.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
-import VirtualDom exposing (Node, Property, program)
-import Json.Encode as Json
+import VirtualDom.Element exposing (Element, column, label, program)
+import VirtualDom.Attributes exposing (flexGrow, justifyContent, alignItems, text)
 import Time exposing (Time, second)
 
 
@@ -47,16 +47,14 @@ update msg model =
 -- VIEW
 
 
-view : Model -> Node Msg
+view : Model -> Element Msg
 view model =
     column
         [ flexGrow 1
         , justifyContent "center"
         , alignItems "center"
         ]
-        [ label
-            [ text (toString model)
-            ]
+        [ label [ text (toString model) ]
         ]
 
 
@@ -67,57 +65,3 @@ view model =
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Time.every second Tick
-
-
-
--- UIKit Definitions
-
-
-label : List (Property msg) -> Node msg
-label properties =
-    VirtualDom.leaf "label" properties
-
-
-column : List (Property msg) -> List (Node msg) -> Node msg
-column properties children =
-    VirtualDom.parent "view" (flexDirection "column" :: properties) children
-
-
-stringProperty : String -> String -> Property msg
-stringProperty name string =
-    VirtualDom.property name (Json.string string)
-
-
-stringYogaProperty : String -> String -> Property msg
-stringYogaProperty name string =
-    VirtualDom.yogaProperty name (Json.string string)
-
-
-intYogaProperty : String -> Int -> Property msg
-intYogaProperty name int =
-    VirtualDom.yogaProperty name (Json.int int)
-
-
-text : String -> Property msg
-text value =
-    stringProperty "text" value
-
-
-flexDirection : String -> Property msg
-flexDirection value =
-    stringYogaProperty "flexDirection" value
-
-
-flexGrow : Int -> Property msg
-flexGrow value =
-    intYogaProperty "flexGrow" value
-
-
-justifyContent : String -> Property msg
-justifyContent value =
-    stringYogaProperty "justifyContent" value
-
-
-alignItems : String -> Property msg
-alignItems value =
-    stringYogaProperty "alignItems" value
